6d43b5c374cfc5b324adc7fbd7877111f9b8417c
b87480 <jwhitney114@fullsail.edu>
2013-03-21 21:05:42 -0400

    Finished Methods and started story line
    Had to start all over due to technical issues!

--- Whitney_Joshua.js
+++ Whitney_Joshua.js
@@ -6,31 +6,14 @@ March 19, 2013
 */
 
 
+//Global Variabels
+var hitDamage = 5;
+var yourStrike = Math.floor(Math.random() * 2);
+var totalDamage = 0;
+var damageThisRound = Math.floor(Math.random() * 5 + 1);
+var combat = true;
+//End Global Variables
 
-//HEROES OBJECT
-var myhero = {
-	
-	"name"		:"NONE",
-	"age"		:00,
-	"weapon"	:"NONE",
-	"vehicle"	:"NONE",
-	"health"	:00,
-	"hitPoints" :function(number) { //Accessor Method
-		var number = myhero.health
-		return number;
-				
-	},//end Accessor Method
-	"newHero"	:function(name, age, weapon, vehicle) { //Processor Method
-		myhero.name = name;
-		myhero.age = age;
-		myhero.weapon = weapon;
-		myhero.vehicle = vehicle;
-	}//end Mutator Method
-
-	
-};
-myhero.newHero("Josh", 28, "Rifle", "Hummer");
-console.log(myhero.hitPoints());
 //JSON DATA
 var zombie = {
 	"zombies": [
@@ -69,11 +52,69 @@ var zombie = {
 	]
 };
 
-var zombieIntro = function() {
-	for(var i = 0; i < zombie.zombies.length; i++) {
-		var friend = zombie.zombies[i];
-			console.log('My name is ' + friend.name + ' , I am ' + friend.age + ' years old.' + ' I use a ' + 
-						friend.weapon + ' for a weapon.');
-	};
-};
+//Start Hero OBJECT
+var myhero = {
+	
+			"name"		:"NONE",
+			"age"		:00,
+			"weapon"	:"NONE",
+			"vehicle"	:"NONE",
+			"health"	:5,
+
+	"hitPoints" :function(number) { //Accessor Method
+		var number = myhero.health;
+		return number;
+				
+	},//end Accessor Method
+
+	"newHero"	:function(name, age, weapon, vehicle) { //Process Method
+		myhero.name = name;
+		myhero.age = age;
+		myhero.weapon = weapon;
+		myhero.vehicle = vehicle;
+		
+	},//end Process Method
+	
+	"checkAge"	:function() { // Function Method
+		if (myhero.age >= 18) {
+			console.log("You are old enough to fight the zombies!");
+		} else {
+			console.log("You are to young to fight zombies!");
+		}
+	},//end function Method
+	
+	"newHealth"	:function() { // Mutator Method	
+		myhero.health = myhero.hitPoints() * 10;
+		
+	},//end Mutator Process
+
+} //end Hero Object
+
+//Start Script
+myhero.newHero("Josh",32,"axe","tank");
+console.log("You are a hero named " + myhero.name + ". You are " + myhero.age + ". You use a " 
+			+ myhero.weapon + " to fight off zombies while riding around in your " + myhero.vehicle + "!");
+
+console.log("There are four zombies that you must defeat before the destroy your town!");			
+			
+			var zombieIntro = function() {
+				for(var i = 0; i < zombie.zombies.length; i++) {
+					var creature = zombie.zombies[i];
+					console.log(creature.name + ', and he is ' + creature.temper + "."); 
+				};
+			};
 zombieIntro();
+
+console.log("First we need to check if you are old enough to fight off zombies.");
+myhero.checkAge();
+
+console.log("Next we have to make sure that your health is up too par.");
+console.log("You currently have " + myhero.hitPoints() + " hit points!");
+
+			
+
+
+
+
+
+
